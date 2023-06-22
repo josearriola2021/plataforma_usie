e97ae07aa6237ccfcd91c8477aff9e832355faf0
josearriola2021 <joseluisperez1718@gmail.com>
2023-06-22 11:51:35 -0500

    commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -64,7 +64,7 @@ const Card = ({producto}) => {
       </Link>
       <div className="card-body flex-none">
         <h2 className="card-title text-base">{nombre}</h2>
-        <p className="text-red-500 font-bold text-base">{precio}%</p>
+        <p className="text-red-500 font-bold text-base">Avance: {precio}%</p>
         <div className="card-actions justify-end agregar-button">
           {buttonSecondary ? (
             <Button
@@ -73,7 +73,7 @@ const Card = ({producto}) => {
               size={size}
               onClick={activeButtonSecondary}
             >
-              Agregar
+              Ver más
             </Button>
           ) : (
             <Button
